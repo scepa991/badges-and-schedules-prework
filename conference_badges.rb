32892ba6a444286e0b90b6fd268829082a96cc97
Stefan Scepanovic <scepa91@icloud.com>
2019-05-09 22:49:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,3 +3,12 @@ people = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
+
+# BATCH BADGE CREATOR
+def batch_badge_creator(array)
+  nuarray = []
+  array.each do |name|
+    nuarray.push("Hello, my name is #{name}.")
+  end 
+  return nuarray
+end
